8073db85bd2f4e3b9b9012d2268cfa2bea15ba2b
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-05-18 13:07:54 +0200

    add init GUI concept (is it necessary??)

--- core/src/main/scala/it/unibo/core/Boundary.scala
+++ core/src/main/scala/it/unibo/core/Boundary.scala
@@ -4,6 +4,7 @@ import monix.eval.Task
 import monix.reactive.Observable
 
 trait Boundary[-Model, +Input] {
+  def init(): Task[Unit] = Task.pure {}
   def input: Observable[Input]
   def render(model: Model): Task[Unit]
 }
